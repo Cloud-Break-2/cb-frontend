6c8a10d9b046405b18ed3e50a9d890490c6928c7
chaeminseok <minseok23491@naver.com>
2024-08-26 01:36:07 +0900

    feat: test jenkins

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -50,7 +50,7 @@ const Popup = ({ isOpen, onClose }) => {
           allowFullScreen></iframe>
         <div className="kakao-button-wrapper">
           <div className="kakao-button" onClick={onClickEventPage}>
-            네고왕 X 카카오 이벤트 바로가기
+            네고왕왕 X 카카오 이벤트 바로가기
           </div>
         </div>
         <div className="today-button" onClick={onClose}>
